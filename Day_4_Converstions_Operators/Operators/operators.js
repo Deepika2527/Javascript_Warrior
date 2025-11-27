f3d5e9f4e691ad4ba772e0963246173882473980
Deepika2527 <deepikatrainings.com@gmail.com>
2025-11-27 18:21:59 +0530

    Added Operators

---
+++ Day_4_Converstions_Operators/Operators/operators.js
@@ -0,0 +1,264 @@
+// Arthemativ-  + , -, * , /, %, **
+
+
+
+// let n1 = 20;
+// let n2 = 10;
+// console.log('Addition:', n1+n2);
+// console.log('Subtraction', n1-n2);
+// console.log('MUltiplication', n1*n2);
+// console.log('Expoential', n1**2);
+// console.log('Division', n1/n2);
+// console.log('Modulus', n1%n2);
+
+
+//Comparsions Operator
+
+// console.log("We are learing on Comparastion");
+// console.log(n1<n2);
+// console.log(n2>n1);
+// console.log(10 <= 10);
+// console.log(100 >= 20);
+
+// console.log(10 == 10);
+// console.log(10 === 10);
+
+// console.log(10 == "10" );
+// console.log(10 === "10");
+
+
+// console.log(true == true);
+// console.log(true === true);
+// console.log(NaN == NaN);
+// console.log(NaN === NaN);
+// console.log(undefined == undefined);
+// console.log(undefined === undefined);
+
+// let a = true
+// console.log(!a);
+// let b= 10;
+// console.log(!b);
+// console.log(!true);
+// console.log(!false);
+// console.log(!1);
+// console.log(!0);
+// console.log(!"hello");
+// console.log(!"");
+
+
+// // !=
+
+
+// console.log(5 == 5);
+// console.log(5 === 5);
+// console.log("***");
+
+// console.log(5 == "5");
+// console.log(5 === "5");
+// console.log("***");
+// console.log(5 != 5);
+// console.log(5 !== 5);
+// console.log("***");
+// console.log(5 != "5");
+// console.log(5 !== "5");
+
+// console.log("****");
+
+// console.log(true != 1);
+// console.log(true !== 1);
+
+
+// Assigment
+// let a = 10;
+// console.log(a+a);
+// console.log(a+=a);
+// a = a+a
+
+// let b= 20;
+// console.log(a+b);
+
+
+// console.log(a+=b);
+
+
+
+// let n1=5;
+// let n2=10;
+// console.log('Addition', n1+=n2);
+// console.log('Subraction', n1-=n2);
+// console.log(n1 - n2);
+// console.log(n2 - n1);
+
+
+//n1 = n1 -n2
+// console.log('Subraction', n2-=n1);
+
+
+
+// console.log(n1*= n2);
+
+
+
+//UnaryOperator
+
+//postIncremnt
+// let num = 10;
+// console.log(num++);
+// console.log(num);
+
+
+
+// let num1 = 3;
+// console.log(++num1);
+// console.log(num1);
+
+// let nm = 10;
+// console.log(nm++); //10
+// console.log(nm); //11
+// console.log(++nm); //12  
+// console.log(nm++);  //12
+// console.log(nm++ + ++nm +nm++);
+
+
+// let a= 2;
+
+// console.log(--a); //1
+// console.log(a--);  //1 ///0
+// console.log(a--);  //0    -1 
+// console.log(a--);  // -1 -2
+// console.log(--a);  
+
+
+//Logical
+// let age = 18;
+// let hasId = false;
+
+
+// console.log(age>=18 && hasId);
+// console.log(age>18 || hasId);
+
+
+
+// console.log(true && true);
+// console.log(true && false);
+// console.log(false && true);
+// console.log(false && false);
+
+
+// console.log(3>5 || 5<8);
+
+// console.log(3>5 && "Deepika");
+
+// console.log(3>5 || "Deepika");
+
+
+// console.log(true && false);
+// console.log(true && false || true);
+
+
+
+// let logIn = true;
+// console.log(logIn);
+// console.log(!logIn);
+
+
+// let isNight = true;
+
+// if(!isNight){
+//     console.log('Turn off the lights');
+    
+// }
+// else{
+//     console.log('Turn on the light');
+    
+// }
+
+
+//ternary
+
+
+let age = 18;
+let result = age>=18 ? "Adult" : "Minor";
+console.log(result);
+
+
+let n= 10;
+
+console.log(n %2 ===0? "Even" : "Odd");
+// console.log(n);
+
+
+
+let marks = 5;
+
+let res = marks >= 90 ? "A" : "B";
+console.log(res);
+let res1= marks >= 90 ? "A" : marks >= 75 ? "B" : marks >=35 ? "JustPass" : "Fail";
+console.log(res1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// nm++ + ++nm + nm++ = ?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Day_4_Converstions_Operators/converstions/index.js
@@ -0,0 +1,131 @@
+let a = 10;
+let b = "20";
+
+
+let c = a+b;
+console.log(c);
+console.log(typeof(c));
+
+
+let d = a*b;
+console.log(d);
+console.log(typeof(d));
+
+
+let e = a/b;
+console.log(e);
+console.log(typeof(e));
+
+
+
+console.log(('10'));
+console.log(typeof('10'));
+console.log(Number('10'));
+
+let n1 = Number('10');
+console.log(n1);
+console.log(typeof(n1));
+
+console.log(Number('false'));  //Nan
+console.log(Number('')); //0
+console.log(Number('0'));
+console.log(Number([]));
+console.log(Number({  }));
+console.log(Number('   '));
+console.log(Number('undefined'));
+console.log(Number(undefined));
+console.log(Number(null));
+console.log(Number(123.90));
+console.log(Number("123.90rt"));
+
+
+
+
+console.log(String('Hello'));
+console.log(String('1234'));
+console.log('1234');
+console.log(undefined);
+console.log(String(undefined));
+console.log('undefined');
+console.log(false);
+console.log(String(false));
+console.log(String("123er"));
+
+
+
+
+console.log(Boolean(1));
+console.log(1);
+console.log(Boolean(''));
+console.log(Boolean(' '));
+console.log(Boolean([]));
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+
+
+
+
+console.log(parseInt('123.56'));
+console.log(Number('123.65'));
+console.log(parseInt('123.56ty'));
+console.log(Number('123.65ty'));
+
+
+//nan
+console.log(null + null);
+console.log(undefined + undefined);
+console.log(undefined + null);
+console.log(0 /0);
+console.log(undefined + 5);
+console.log(NaN === NaN);
+console.log(typeof(NaN));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(Number(typeof('10')));
+
+
+
+
+
+
