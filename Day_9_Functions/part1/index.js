7ec5ce0cc1033c5edbd92f66617adea141abb052
Deepika2527 <deepikatrainings.com@gmail.com>
2025-12-05 18:51:10 +0530

    FunctionIntro

---
+++ Day_9_Functions/part1/index.js
@@ -0,0 +1,116 @@
+// console.log("Hello Anu");
+// console.log("Hello Anu");
+// console.log("Hello Anu");
+// console.log("Hello Anu");
+// console.log("Hello Anu");
+// console.log("Hello Anu");
+// console.log("Hello Anu");
+// console.log("Hello Anu");
+// console.log("Hello Anu");
+// console.log("Hello Anu");console.log("Hello Anu");
+
+
+
+
+// function greet(){
+//   console.log("Hello Good Evening Anu...");
+  
+// } Function declaration
+// greet()
+// function greet(){
+//   console.log("Hello Good Evening Anu...");
+  
+// }
+// greet();
+// greet();
+
+
+// function prnt(){
+//     console.log("********");
+//     // return "hello"
+    
+// }
+// prnt();
+// console.log(prnt);
+// console.log(prnt()); //undefined
+// console.log(typeof(prnt));   //function
+// console.log(typeof(prnt())); //undefined 
+
+
+
+// function add(a,b){
+//   console.log(a+b);
+  
+  
+// }
+// add(10,10);
+
+
+
+function greeting(name){
+    console.log(`Hello ${name}`);
+    
+
+}
+greeting('Anuradha')
+greeting("Deepika")
+
+
+//function default parameter
+function mul(n=10,m=20){
+    console.log(n*m);
+    
+}
+mul();
+function mulp(n1=10,n2=20){
+    console.log(n1*n2);
+    
+}
+mulp(n1=20,n2=30);
+
+
+function sub(a1=20,b1){
+    console.log(a1-b1);
+    
+}
+sub(10,5);
+
+console.log("*****");
+//more parameter and less arguments
+function subs(a=20,b=1,c){
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    
+    
+    
+}
+subs(10,5,)
+
+//more arguments less paraemters
+
+function disp(l,m,n){
+    console.log(l);
+    console.log(m);
+    
+    console.log(n);
+    
+    
+    
+}
+disp(10,10,'a','hello')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
